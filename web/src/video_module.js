cb82ccd1113370924de8404630827e3d937eee0b
Huangsam894 <huangsam0225@gmail.com>
2021-08-17 15:59:38 +0800

    add select conponer

--- web/src/video_module.js
+++ web/src/video_module.js
@@ -10,10 +10,13 @@ class VideoModule extends Component {
         this.state = {
             //
             selectNamesOptions: [],
-            selected: ""
+            selectOptions: [],
+            selected: "1",
+            selected_2: "2"
         };
         //
         this.selectedHandleChange = this.selectedHandleChange.bind(this);
+        this.change = this.change.bind(this);
     }
 
     // load view on start
@@ -31,15 +34,30 @@ class VideoModule extends Component {
         this.setState({ selectNamesOptions: options });
     }
 
+    // Get Select Options
+    async getOptions() {
+        const options = [
+            { value: '0', label: 'Chocolate' },
+            { value: '1', label: 'Strawberry' },
+            { value: '2', label: 'Vanilla' }
+        ]
+        this.setState({ selectOptions: options });
+    }
     async selectedHandleChange(e) {
         const selected = "此選項編號:" + e.value + "您選擇的是:" + e.label
+        this.getOptions()
         this.setState({ selected: selected });
+    }
+    async change(e) {
+        const selected = "此選項編號:" + e.value + "您選擇的是:" + e.label
+        this.setState({ selected_2: selected });
     }
 
     render() {
 
         const {
             selectNamesOptions,
+            selectOptions,
             selected
         } = this.state;
 
@@ -54,10 +72,16 @@ class VideoModule extends Component {
                                 options={selectNamesOptions}
                                 onChange={this.selectedHandleChange}
                             />
+                            <Select
+                                placeholder="123456"
+                                options={selectOptions}
+                                onChange={this.change}
+                            />
                         </div>
 
                         <div class="col-8 bg-info text-center text-white">
                             <label>{selected}</label>
+                            <label>{selected_2}</label>
                         </div>
 
                     </div>
